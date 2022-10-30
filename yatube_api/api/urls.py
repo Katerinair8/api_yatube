fc70731d188dc0b3d0e453a2e6dd882e0ec789ec
Екатерина Василевич <katerinair8@mail.ru>
2022-10-30 15:58:42 +0100

    sprint 8. final project

---
+++ yatube_api/api/urls.py
@@ -0,0 +1,31 @@
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
+
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+
+from .views import PostViewSet, GroupViewSet, CommentViewSet, UserViewSet
+
+app_name = 'api'
+
+router_v1 = DefaultRouter()
+
+router_v1.register(r'users', UserViewSet, basename='users')
+router_v1.register(r'posts', PostViewSet, basename='posts')
+router_v1.register(r'groups', GroupViewSet, basename='groups')
+router_v1.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
+
+
+urlpatterns = [
+    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/', include(router_v1.urls)),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
---
+++ yatube_api/api/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
+from rest_framework import viewsets
+from rest_framework import mixins 
+from rest_framework.permissions import AllowAny
+
+from posts.models import Post, Group, User
+from .serializers import PostSerializer, GroupSerializer, CommentSerializer, UserSerializer
+
+class PostViewSet(viewsets.ModelViewSet):   
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(PostViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(PostViewSet, self).perform_destroy(instance)
+
+
+class UserViewCreateSet(mixins.CreateModelMixin, mixins.ListModelMixin,
+                        mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    pass
+
+class UserViewSet(UserViewCreateSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
+        return post.comments
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
+        serializer.save(author=self.request.user, post=post)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(CommentViewSet, self).perform_update(serializer)
+    
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        super(CommentViewSet, self).perform_destroy(instance)
